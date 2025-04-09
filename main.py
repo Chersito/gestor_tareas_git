376d492c986203ab1e6bb4c2e2f157c1e10447b4
charly <charly@localhost.localdomain>
2025-04-09 16:51:15 -0600

    Cambios de la rama mejoras

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-print("Hola Mundo")
+print("=== Bienvenido al Gestor de Tareas ===\n")
+print("[1] Agregar una tarea")
+print("[2] Ver todas las tareas")
 
-with open("tareas.txt", "a") as archivo: 
-    tarea = input("Escribe una nueva tarea: ") 
-    archivo.write(tarea + "\n") 
-    print("Tarea guardada.")
+opcion = input("\nElige una opción: ")
 
-print("\nTus tareas:") 
-
-with open("tareas.txt", "r") as archivo: 
-    print(archivo.read())
+if opcion == "1":
+    with open("tareas.txt", "a") as archivo: 
+        tarea = input("[+] Escribe una nueva tarea: ") 
+        archivo.write(tarea + "\n") 
+        print("[i] Tarea guardada.")
+elif opcion == "2":
+    print("\nTus tareas:")
+    try:
+        with open("tareas.txt", "r") as archivo:
+            contenido = archivo.read()
+            if contenido.strip() == "":
+                print("[!] No hay tareas registradas.")
+            else:
+                print(contenido)
+    except FileNotFoundError:
+        print("[!] Aún no hay tareas registradas.")
+else:
+    print("[!] Opción no válida. Intenta nuevamente.")
